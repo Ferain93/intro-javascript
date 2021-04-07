681073d3242e512ea2ce8c55620bf1a49565f326
Ferain93 <fer.tn7@gmail.com>
2021-04-07 00:55:58 -0500

    feat: first steps and basic operators

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-//Comentario de una linea
-/*Comentarios de 
-varias lineas
+//One line comments
+/*Milti-line 
+comments
 */
+//first steps
 console.log("Hi Gago")
 console.log("I Love U")
 
 alert("Hi")
+
+//basic operators
+console.log(5 + 3 -9)
+
+//exponent 
+console.log(3 ** 2)
+
+//square root 
+console.log(25 ** 0.5)
+Math.sqrt(36)
