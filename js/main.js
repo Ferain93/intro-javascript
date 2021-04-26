0b597a469dd00e2f7b557b3195e006ce6470cc7f
Ferain93 <fer.tn7@gmail.com>
2021-04-26 14:38:38 -0500

    feat: steps with console

--- js/main.js
+++ js/main.js
@@ -2,11 +2,31 @@
 /*Milti-line 
 comments
 */
-//first steps
+//first steps with console
 console.log("Hi Gago")
 console.log("I Love U")
 
+/*Change style all h2 in console of Chrome -->*///document.querySelector['h2'].style.color = 'blue' 
+
 alert("Hi")
+
+var curso = "Curso de Javascript";
+console.log("Nombre del curso: ", curso);
+
+console.log([1,2,4,5,6,7,4,4]);
+
+console.log({a:288, b:378, c:233})
+
+console.table({a:288, b:378, c:233})
+
+console.error("Este es un error");
+console.warn('Esta es una advertencia');
+
+console.clear();
+
+console.time('curso');
+//Show the time 
+console.timeEnd('curso');
 
 //basic operators
 console.log(5 + 3 -9)
@@ -16,4 +36,4 @@ console.log(3 ** 2)
 
 //square root 
 console.log(25 ** 0.5)
-Math.sqrt(36)
+Math.sqrt(36);
